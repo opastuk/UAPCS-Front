2d0983f935280a184227475da3811a555e67fb30
Никита Павленко <nick.pavlenko1998@gmail.com>
2020-05-03 12:40:36 +0300

    stage подключение шрифтов

--- src/App.vue
+++ src/App.vue
@@ -9,4 +9,16 @@
     height: 100%;
     width: 100%;
   }
+
+  @font-face {
+    font-family: "Roboto regular";
+    src: url("/assets/fonts/Roboto-Regular.ttf");
+    font-weight: 400;
+  }
+
+  @font-face {
+    font-family: "Roboto Bold";
+    src: url("/assets/fonts/Roboto-Bold.ttf");
+    font-weight: 900;
+  }
 </style>
