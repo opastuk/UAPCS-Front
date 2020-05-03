fc8f4bc0e647d1a0d0b5b47d8b80faa55811bdee
Никита Павленко <nick.pavlenko1998@gmail.com>
2020-05-03 16:51:25 +0300

    feat_api класс запроса api

---
+++ src/api/get.js
@@ -0,0 +1,20 @@
+import axios from 'axios';
+import api from '../../config/api.json'
+
+const CONFIG = JSON.parse(api);
+
+
+export default class Api {
+  constructor() {
+    this.host = `http://${CONFIG.host}:${CONFIG.port}`;
+  }
+
+
+
+  hospitals () {
+    axios.get(`${this.host}/hospitals`).then((response) => {
+      return response.data
+    })
+  }
+}
+
