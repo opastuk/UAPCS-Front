035a0944412a3cd7b8d5aa6b29c8ca6119bca8dd
Никита Павленко <nick.pavlenko1998@gmail.com>
2020-05-03 12:07:57 +0300

    feat_layout-grid исправление роутера

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 import Home from '../views/Home.vue';
-import Auth from '../views/Auth';
+import Auth from '../views/AuthPage.vue';
 
 Vue.use(VueRouter);
 
