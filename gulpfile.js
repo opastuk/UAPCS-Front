c56418f738de3f386162b215730c8494c00eeb34
Никита Павленко <nick.pavlenko1998@gmail.com>
2020-05-03 21:32:28 +0300

    feat_sprite настроен галп для сбора спрайта иконок

---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+const svgSprite = require('gulp-svg-sprites');
+const gulp = require('gulp');
+
+gulp.task('svgSprite', function () {
+  return gulp.src('./public/img/icons/*.svg') // svg files for sprite
+    .pipe(svgSprite({
+        mode: {
+          stack: {
+            sprite: "../sprite.svg"  //sprite file name
+          }
+        },
+      }
+    ))
+    .pipe(gulp.dest('./public/img/icons/'));
+});
