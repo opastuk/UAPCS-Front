d01b41848036b93ae962338e70fc6c4f5578ca99
Никита Павленко <nick.pavlenko1998@gmail.com>
2020-05-03 12:33:56 +0300

    feat_layout-grid импорт нормалайза

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 import './registerServiceWorker';
+import 'normalize.css';
 
 Vue.config.productionTip = false;
 
