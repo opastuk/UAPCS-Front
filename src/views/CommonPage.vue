d4841574f6b72f2b62480a5a52e40cdf548649be
Никита Павленко <nick.pavlenko1998@gmail.com>
2020-05-03 12:32:47 +0300

    feat_layout-grid установка нормалайза и накидывание компонентов

---
+++ src/views/CommonPage.vue
@@ -0,0 +1,18 @@
+<template>
+	<div>
+		<component></component>
+	</div>
+</template>
+
+<script>
+	import { Vue, Component} from 'vue-property-decorator'
+
+	@Component({})
+  export default class CommonPage extends Vue {
+
+  }
+</script>
+
+<style scoped lang="scss">
+
+</style>
